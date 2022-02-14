6a11b561263e2953ec6be2117ca6e4d8039d54fe
KARMA <61666807+SomeRandomTV@users.noreply.github.com>
2022-02-14 07:54:45 -0700

    Create player1Class.cpp

---
+++ player1Class.cpp
@@ -0,0 +1,23 @@
+//
+//  Player Class.cpp
+//  Monoply
+//
+//  Created by Retr0 on 2/9/22.
+//
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <cstdlib>
+#include <vector>
+using namespace std;
+
+
+class Player1 {
+public:
+    int money = 1500;
+    
+    vector<string> propertiesOwned();
+    
+    
+    
+};
